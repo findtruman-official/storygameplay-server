1794b0042f0786c7a763fc39c043b106f0a2d392
tasse00 <tasse_00@163.com>
2022-06-27 10:15:59 +0800

    feat: add swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const logger = new Logger();
 
+async function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('FindTruman Co-Creation Api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+  logger.log('Swagger API Doc enabled.');
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const svc = app.get(ConfigService);
-  const port = svc.get<number>('PORT', { infer: true });
+  const configService = app.get(ConfigService);
+
+  const swaggerEnabled = configService.get<boolean>('SWAGGER_ENABLED', {
+    infer: true,
+  });
+  if (swaggerEnabled) {
+    setupSwagger(app);
+  }
+
+  const port = configService.get<number>('PORT', { infer: true });
   await app.listen(port, () => {
     logger.log(`server listen at ${port}`);
   });
