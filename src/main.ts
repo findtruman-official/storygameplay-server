e72d334f571689fb161aef0540a94d1e2075e93f
tasse00 <tasse_00@163.com>
2022-06-27 09:52:33 +0800

    update: use config to set listen port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const svc = app.get(ConfigService);
+  const port = svc.get<number>('PORT', { infer: true });
+  await app.listen(port, () => {
+    logger.log(`server listen at ${port}`);
+  });
 }
 bootstrap();
